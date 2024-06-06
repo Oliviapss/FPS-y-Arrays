5e50bb29e116a27f4bd5bdb1982624b8ed08dcc1
Oliviapss <olivia1009palomino@hotmail.com>
2024-06-06 09:10:38 -0300

    Initial commit

---
+++ Assets/Alimentos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alimentos : MonoBehaviour
+{
+    public int valoralimentario;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Personajes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personajes : MonoBehaviour
+{
+    public int nivelHambre;
+    public Alimentos alimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TomarAlimento(int valoralimentario)
+    {
+        nivelHambre -= valoralimentario;
+        if (nivelHambre <= 0)
+        {
+            nivelHambre = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        Alimentos alimento;
+        alimento = other.GetComponent<Alimentos>();
+        TomarAlimento(alimento.valoralimentario);
+    }
+}
+
